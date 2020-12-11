1508b6e310538df94ff622aae3b00843e20bd500
DoVietDuc <ducdv@pixelz.com>
2020-12-11 18:12:10 +0700

    [QA-Structure] init maven project

---
+++ templates/QATemplates/src/main/java/QA/Templates/App.java
@@ -0,0 +1,13 @@
+package QA.Templates;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
