468fbdc8f5b1a6bd8c3a73edb3c2e964501f088a
DoVietDuc <ducdv@pixelz.com>
2020-12-18 16:44:07 +0700

    [QA-Structure] TestNG - add test suite

--- templates/AutoQATemplates/src/test/java/vn/qa/automation/pages/pageLogin/LoginRunner.java
+++ templates/AutoQATemplates/src/test/java/vn/qa/automation/pages/pageLogin/LoginRunner.java
@@ -2,18 +2,22 @@ package vn.qa.automation.pages.pageLogin;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class LoginRunner {
-  @Test
-  public void f() {
-  }
-  @BeforeClass
-  public void beforeClass() {
-  }
 
-  @AfterClass
-  public void afterClass() {
-  }
+	@Test
+	public void f() {
+		Assert.assertTrue(true);
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
 
 }
